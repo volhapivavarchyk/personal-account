1b1973365f6d49d57b10e65f7c21614411d8ec84
pivovarchyk <pivovarchk@gmail.com>
2020-01-29 17:30:24 +0300

    changed

--- app/personal-account/assets/js/app.js
+++ app/personal-account/assets/js/app.js
@@ -7,3 +7,13 @@ import 'bootstrap-sass/assets/javascripts/bootstrap/collapse.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/dropdown.js';
 import 'bootstrap-sass/assets/javascripts/bootstrap/modal.js';
 import 'jquery'
+
+$(document).ready(function() {
+    $('.addauthor').click(function() {
+        console.log('111');
+        let div = document.createElement('div');
+        div.id = 'soathor1';
+        div.appendChild(document.getElementById('soauthors'));
+        div.innerText = '1111';
+    });
+});
