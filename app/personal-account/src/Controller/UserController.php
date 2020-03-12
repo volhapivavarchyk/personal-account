96916226ed3db2761bc209d22ffcbcf1c13a7f9b
pivovarchyk <pivovarchk@gmail.com>
2020-03-12 17:13:52 +0300

    changed

--- app/personal-account/src/Controller/UserController.php
+++ app/personal-account/src/Controller/UserController.php
@@ -14,9 +14,24 @@ class UserController extends AbstractController
     /**
      * @Route("/", methods="GET|POST", name="homepage")
      */
-    public function action(Request $request): Response
+    public function authorization(Request $request): Response
+    {
+        return $this->render('default/authorization.html.twig',[]);
+    }
+
+    /**
+     * @Route("/personal/{module}/{service}", methods="GET|POST", name="personal")
+     */
+    public function action(Request $request, int $module = 0, int $service = 0): Response
     {
         //return $this->render('default/authorization.html.twig',[]);
-        return $this->render('users/user.html.twig',[]);
+        return $this->render(
+            'users/user.html.twig',
+            [
+                'module' => $module,
+                'service' => $service
+            ]
+        );
     }
+
 }
